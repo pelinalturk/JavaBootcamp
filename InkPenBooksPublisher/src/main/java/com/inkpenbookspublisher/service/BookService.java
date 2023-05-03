3df8cb98876161fc0ed08ac006f424265fb3df1c
pelinalturk <80326351+pelinalturk@users.noreply.github.com>
2023-05-03 22:35:08 +0300

    Add methods for retrieving, checking existence, and deleting books by ID

--- InkPenBooksPublisher/src/main/java/com/inkpenbookspublisher/service/BookService.java
+++ InkPenBooksPublisher/src/main/java/com/inkpenbookspublisher/service/BookService.java
@@ -4,8 +4,13 @@ import com.inkpenbookspublisher.model.dto.BookDto;
 import com.inkpenbookspublisher.model.request.CreateBookRequest;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookService {
     BookDto saveBook(CreateBookRequest createBookRequest);
     List<BookDto>getAllBooks();
+    Optional<BookDto>getBookById(String id);
+    void deleteBookById(String id);
+    boolean existsByBookId(String id);
+    Optional<List<BookDto>>getBooksByAuthorId(String authorId);
 }
--- InkPenBooksPublisher/src/main/java/com/inkpenbookspublisher/service/impl/BookServiceImpl.java
+++ InkPenBooksPublisher/src/main/java/com/inkpenbookspublisher/service/impl/BookServiceImpl.java
@@ -3,32 +3,80 @@ package com.inkpenbookspublisher.service.impl;
 import com.inkpenbookspublisher.model.Book;
 import com.inkpenbookspublisher.model.dto.BookDto;
 import com.inkpenbookspublisher.model.request.CreateBookRequest;
+import com.inkpenbookspublisher.service.AuthorService;
 import com.inkpenbookspublisher.service.BookEntityService;
 import com.inkpenbookspublisher.service.BookService;
 import static com.inkpenbookspublisher.model.mapper.BookMapper.BOOK_MAPPER;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class BookServiceImpl implements BookService {
 
     private final BookEntityService bookEntityService;
+    private final AuthorService authorService;
 
-    public BookServiceImpl(BookEntityService bookEntityService) {
+    public BookServiceImpl(BookEntityService bookEntityService, AuthorService authorService) {
         this.bookEntityService = bookEntityService;
+        this.authorService = authorService;
     }
 
     @Override
     public BookDto saveBook(CreateBookRequest createBookRequest) {
         Book book = BOOK_MAPPER.createBook(createBookRequest);
-        bookEntityService.save(book);
-        return BOOK_MAPPER.convertToBookDto(book);
+        if(authorService.existsByAuthorId(book.getAuthorId())){
+            bookEntityService.save(book);
+            return BOOK_MAPPER.convertToBookDto(book);
+        }
+        //exception
+        return null;
     }
 
     @Override
     public List<BookDto> getAllBooks() {
         return bookEntityService.findAll().stream().map(BOOK_MAPPER::convertToBookDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<BookDto> getBookById(String id) {
+        Optional<Book> book = bookEntityService.getById(id);
+        if (book.isPresent()) {
+            return Optional.ofNullable(BOOK_MAPPER.convertToBookDto(book.get()));
+        }
+        //exception
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteBookById(String id) {
+        Optional<Book>book = bookEntityService.getById(id);
+        String authorId = book.get().getAuthorId();
+        if (book.isPresent()) {
+            bookEntityService.delete(id);
+            Optional<List<Book>>getBookByAuthorId = bookEntityService.getBookByAuthorId(authorId);
+            if(getBookByAuthorId.get().size()==0){
+                authorService.deleteAuthorById(authorId);
+            }
+        }
+    }
+
+    @Override
+    public boolean existsByBookId(String id) {
+        return bookEntityService.existsById(id);
+    }
+
+    @Override
+    public Optional<List<BookDto>> getBooksByAuthorId(String authorId) {
+        Optional<List<Book>> bookList = bookEntityService.getBookByAuthorId(authorId);
+        if (authorService.existsByAuthorId(authorId)) {
+            if (bookList.isPresent()) {
+                return Optional.ofNullable(bookList.get().stream().map(BOOK_MAPPER::convertToBookDto).collect(Collectors.toList()));
+            }
+        }
+        //author id not exists exception
+        return null;
     }
 }
