91352aa2cd20b529eb97c6af31be633e0a761fb8
pelinalturk <80326351+pelinalturk@users.noreply.github.com>
2023-05-03 19:37:48 +0300

    create model classes

---
+++ InkPenBooksPublisher/src/main/java/com/inkpenbookspublisher/model/Author.java
@@ -0,0 +1,21 @@
+package com.inkpenbookspublisher.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Document("authors")
+public class Author extends BaseEntity{
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String biography;
+}
---
+++ InkPenBooksPublisher/src/main/java/com/inkpenbookspublisher/model/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.inkpenbookspublisher.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class BaseEntity {
+
+    @Builder.Default
+    private Boolean isDeleted = false;
+
+    @LastModifiedDate
+    private Date lastModified;
+
+    @CreatedDate
+    private Date createdDate;
+}
